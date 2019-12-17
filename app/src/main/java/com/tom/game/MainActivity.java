c96bada8d8fbe9fa07a14e44574c130d0067c900
juan890719 <s0910173680>
2019-12-17 16:20:05 +0800

    goood

--- app/src/main/java/com/tom/game/MainActivity.java
+++ app/src/main/java/com/tom/game/MainActivity.java
@@ -13,25 +13,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button_up).setOnClickListener(this);
-        findViewById(R.id.button_down).setOnClickListener(this);
-        findViewById(R.id.button_left).setOnClickListener(this);
-        findViewById(R.id.button_right).setOnClickListener(this);
+        findViewById(R.id.arrow_up).setOnClickListener(this);
+        findViewById(R.id.arrow_down).setOnClickListener(this);
+        findViewById(R.id.arrow_left).setOnClickListener(this);
+        findViewById(R.id.arrow_right).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.button_up:
+            case R.id.arrow_up:
                 Log.d("MainActivity","onClick_UP");
                 break;
-            case R.id.button_down:
+            case R.id.arrow_down:
                 Log.d("MainActivity","onClick_DOWN");
                 break;
-            case R.id.button_left:
+            case R.id.arrow_left:
                 Log.d("MainActivity","onClick_LEFT");
                 break;
-            case R.id.button_right:
+            case R.id.arrow_right:
                 Log.d("MainActivity","onClick_RIGHT");
                 break;
         }

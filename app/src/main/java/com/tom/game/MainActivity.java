3234008356cf43103c9a85cea01f967f08393fbb
juan890719 <s0910173680>
2019-12-17 15:58:58 +0800

    good

---
+++ app/src/main/java/com/tom/game/MainActivity.java
@@ -0,0 +1,40 @@
+package com.tom.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button_up).setOnClickListener(this);
+        findViewById(R.id.button_down).setOnClickListener(this);
+        findViewById(R.id.button_left).setOnClickListener(this);
+        findViewById(R.id.button_right).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_up:
+                Log.d("MainActivity","onClick_UP");
+                break;
+            case R.id.button_down:
+                Log.d("MainActivity","onClick_DOWN");
+                break;
+            case R.id.button_left:
+                Log.d("MainActivity","onClick_LEFT");
+                break;
+            case R.id.button_right:
+                Log.d("MainActivity","onClick_RIGHT");
+                break;
+        }
+
+    }
+}

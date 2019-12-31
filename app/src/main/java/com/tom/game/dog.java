c444a73d307c327f53ce49c8b5730e6fde80cdff
juan <michelle.70623@gmail.com>
2019-12-31 16:30:40 +0800

    dog.java 備註期末考考試內容

--- app/src/main/java/com/tom/game/dog.java
+++ app/src/main/java/com/tom/game/dog.java
@@ -1,10 +1,34 @@
 package com.tom.game;
 
-public class dog extends Thread{
-    private static final int DIRECTION_UP = 0;
-    private static final int DIRECTION_DOWN = 1;
-    private static final int DIRECTION_RIGHT = 2;
-    private static final int DIRECTION_LEFT = 3;
-    private static final int DIRECTION_NONE = -1;
+public class dog extends Thread {
+    public static final int DIRECTION_UP = 0;
+    public static final int DIRECTION_DOWN = 1;
+    public static final int DIRECTION_RIGHT = 2;
+    public static final int DIRECTION_LEFT = 3;
+    public static final int DIRECTION_NONE = -1;
+
+    private final GameView gameView;
+    int direction = DIRECTION_NONE;
+    int x, y = 0;
+
+    public dog(GameView gameView) {
+        this.gameView = gameView;
+    }
+
+    @Override
+    public void run() {
+        while (direction != DIRECTION_NONE) {
+            switch (direction) {
+                case DIRECTION_UP:
+                    if (y > 60) {
+                        y -= 50;
+                    }
+                    dog.interrupted();
+
+            }
+        }
+    }
 }
+
+
 //期末：圖片效果 + intent Activity切換 + switch + 儲存資料 到Android手機 SharedPreferences 喜好資料 讀取資料 abd工具
